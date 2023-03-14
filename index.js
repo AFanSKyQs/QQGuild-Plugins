f5df730fbb86e1e6c157df70529b1f1b3ed3cf0a
FanSky_Qs <3141865879@qq.com>
2023-03-14 20:11:42 +0800

    更新index，启动时启动文件检测
    更新index，启动时启动文件检测

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import fs from 'fs'
+import {LoadDefaultConfig} from "./config/LoadDefaultConfig.js";
 
 Bot.logger.info('-------------^_^--------------')
-Bot.logger.info(`----QQGuild-Plugin开始载入-----`)
+Bot.logger.info(`----QQGuild-Plugins开始载入-----`)
 Bot.logger.info(`----Author[0]：AFanSKyQs-----`)
-logger.warn(logger.yellow(`---指令：【#开启公域/私域机器人】------`))
-logger.warn(logger.yellow(`发送指令前请先在config/xxx.json中填入公域/私域机器人配置`))
-logger.warn(logger.yellow(`---私域：PrivateGuildConfig 公域：PublicGuildConfig------`))
 const files = fs.readdirSync('./plugins/QQGuild-Plugins/apps').filter(file => file.endsWith('.js'))
 let ret = []
 files.forEach((file) => {
     ret.push(import(`./apps/${file}`))
 })
 ret = await Promise.allSettled(ret)
+await LoadDefaultConfig()
 let apps = {}
 let APackageFanError = 0
 for (let i in files) {
@@ -34,9 +33,9 @@ for (let i in files) {
     }
     apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
-Bot.logger.info(`----QQGuild-Plugins载入完成-----`)
 if(APackageFanError>0){
   logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
   logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
 }
+Bot.logger.info(`----QQGuild-Plugins载入完成-----`)
 export {apps}
